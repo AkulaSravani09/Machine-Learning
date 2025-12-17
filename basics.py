5844c19f65cb728ebae8d8e66d74963da73e4a8d
AkulaSravani09 <sravaniak09@gmail.com>
2025-12-17 10:54:57 +0530

    Add files via upload

--- basics.py
+++ basics.py
@@ -20,6 +20,26 @@ print("Variance:", variance)
 #calculating standard deviation
 std_deviation = variance ** 0.5
 print("standard deviation:", std_deviation)
+#calculating median using bubble sort technique
+arr = [22, 11, 44, 24, 56, 77, 89, 88]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+sorted_arr = bubble_sort(arr.copy())
+n = len(sorted_arr)
+
+if n % 2 == 0:
+    median_bubble = (sorted_arr[n//2 - 1] + sorted_arr[n//2]) / 2
+else:
+    median_bubble = sorted_arr[n//2]
+
+print("Median using Bubble Sort:", median_bubble)
 
 
 
